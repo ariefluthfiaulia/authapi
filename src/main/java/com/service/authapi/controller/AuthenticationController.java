aeea89bb7b169c94519923fe1e43581558a65e07
Arief Luthfi Aulia <30741134+ariefluthfiaulia@users.noreply.github.com>
2024-07-17 01:41:38 +0700

    Add files via upload
    Initial commit

---
+++ src/main/java/com/service/authapi/controller/AuthenticationController.java
@@ -0,0 +1,47 @@
+package com.service.authapi.controller;
+
+import com.service.authapi.dto.LoginResponseDto;
+import com.service.authapi.dto.LoginUserDto;
+import com.service.authapi.dto.RegisterUserDto;
+import com.service.authapi.model.User;
+import com.service.authapi.service.AuthenticationService;
+import com.service.authapi.service.JwtService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/auth")
+@RestController
+public class AuthenticationController {
+    private final JwtService jwtService;
+
+    private final AuthenticationService authenticationService;
+
+    public AuthenticationController(JwtService jwtService, AuthenticationService authenticationService) {
+        this.jwtService = jwtService;
+        this.authenticationService = authenticationService;
+    }
+
+    @PostMapping("/signup")
+    public ResponseEntity<User> register(@RequestBody RegisterUserDto registerUserDto) {
+        User registeredUser = authenticationService.signup(registerUserDto);
+
+        return ResponseEntity.ok(registeredUser);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponseDto> authenticate(@RequestBody LoginUserDto loginUserDto) {
+        User authenticatedUser = authenticationService.authenticate(loginUserDto);
+
+        String jwtToken = jwtService.generateToken(authenticatedUser);
+
+        LoginResponseDto loginResponse = LoginResponseDto.builder()
+                .token(jwtToken)
+                .expiresIn(jwtService.getExpirationTime())
+                .build();
+
+        return ResponseEntity.ok(loginResponse);
+    }
+}
---
+++ src/main/java/com/service/authapi/service/AuthenticationService.java
@@ -0,0 +1,51 @@
+package com.service.authapi.service;
+
+import com.service.authapi.dto.LoginUserDto;
+import com.service.authapi.dto.RegisterUserDto;
+import com.service.authapi.model.User;
+import com.service.authapi.repository.UserRepository;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthenticationService {
+    private final UserRepository userRepository;
+
+    private final PasswordEncoder passwordEncoder;
+
+    private final AuthenticationManager authenticationManager;
+
+    public AuthenticationService(
+            UserRepository userRepository,
+            AuthenticationManager authenticationManager,
+            PasswordEncoder passwordEncoder
+    ) {
+        this.authenticationManager = authenticationManager;
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public User signup(RegisterUserDto input) {
+        User user = User.builder()
+                .fullName(input.getFullName())
+                .email(input.getEmail())
+                .password(passwordEncoder.encode(input.getPassword()))
+                .build();
+
+        return userRepository.save(user);
+    }
+
+    public User authenticate(LoginUserDto input) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        input.getEmail(),
+                        input.getPassword()
+                )
+        );
+
+        return userRepository.findByEmail(input.getEmail())
+                .orElseThrow();
+    }
+}
